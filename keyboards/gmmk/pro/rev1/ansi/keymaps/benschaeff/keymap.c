3c32218379944a57116cce30028203d2886df87c
BenSchaeff <benjamin_schaeffer@yahoo.com>
2022-08-13 20:52:45 -0500

    Update keymap.c
    Updates to keymap

--- keyboards/gmmk/pro/rev1/ansi/keymaps/benschaeff/keymap.c
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/benschaeff/keymap.c
@@ -30,6 +30,7 @@ enum userspace_layers {
 #define CLOSETAB LCTL(KC_W) // Kill current tab
 #define POPOUT LCTL(LSFT(KC_K))
 
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 //      ESC      F1       F2       F3       F4       F5       F6       F7       F8       F9       F10      F11      F12	     Prt           Rotary(Mute)
@@ -49,11 +50,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // it'll be back to normal when you plug it back in.
 
     [QWERTY] = LAYOUT(
-        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR,          KC_MUTE,
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_DEL,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGUP,
-        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_PGDN,
-        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_END,
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_HOME,          KC_MUTE,
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_END,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_DELETE,
+        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_PGUP,
+        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_PGDN,
         KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(FNLAYER),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
@@ -63,65 +64,148 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, SWAP_L ,  MAXI  , SWAP_R , _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET  ,          _______,
         _______, _______,  MINI  , _______, POPOUT,  _______, _______, _______, _______, _______, _______, _______,          _______,          _______,
         _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______,
-        _______, _______, _______,                            _______,                            _______, _______, KC_TRNS, _______, _______, _______
+        _______, _______, _______,                            _______,                            RGB_TOG, KC_TRNS, _______, _______, _______, _______
     ),
 
 
 };
 
 
-bool encoder_update_user(uint8_t index, bool clockwise) {
-    if (get_mods() & MODS_ALT_MASK) {
-        if (clockwise) {
-            tap_code(KC_TAB);
-        } else {
-            tap_code16(S(KC_TAB));
-        }
-    } else if (get_mods() & MODS_CTRL_MASK) {
-        if (clockwise) {
-            tap_code(KC_TAB);
-        } else {
-            tap_code16(S(KC_TAB));
-        }
-    } else if(IS_LAYER_ON(FNLAYER)) {
-        if (clockwise) {
-            tap_code(KC_MEDIA_NEXT_TRACK);
-        } else {
-            tap_code(KC_MEDIA_PREV_TRACK);
-        }
-    } else {
-        if (clockwise) {
-            tap_code(KC_VOLU);
-        } else {
-            tap_code(KC_VOLD);
-        }
+bool encoder_update_user(uint8_t index, bool clockwise)
+{
+  if (get_mods() & MODS_ALT_MASK)
+	{
+			if (clockwise)
+			{
+				tap_code(KC_TAB);
+			}
+			else
+			{
+				tap_code(S(KC_TAB));
+			}
+	}
+	else if (get_mods() & MODS_CTRL_MASK)
+  {
+    if (clockwise)
+    {
+      tap_code(KC_TAB);
     }
-    return false;
+    else
+    {
+      tap_code(S(KC_TAB));
+
+    }
+  }
+  else if(IS_LAYER_ON(FNLAYER))
+    {
+      if(clockwise)
+      {
+        tap_code(KC_MEDIA_NEXT_TRACK);
+      }
+      else
+      {
+        tap_code(KC_MEDIA_PREV_TRACK);
+      }
+    }
+    else
+    {
+      if (clockwise)
+      {
+        tap_code(KC_VOLU);
+      }
+      else
+      {
+        tap_code(KC_VOLD);
+      }
+
+    }
+    return true;
+
+}
+  void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+    switch(get_highest_layer(layer_state))
+    {  // special handling per layer
+            case 0:  //layer one
+                if (host_keyboard_led_state().caps_lock)
+                    rgb_matrix_set_color_all(255,0,0);
+                else
+                    //set all to default
+                 for (uint8_t i = 0; i < DRIVER_LED_TOTAL; i++)
+                    {
+                        RGB_MATRIX_INDICATOR_SET_COLOR(i,32,32,32);
+                    }
+                    //light orange keys
+                    /*
+                    RGB_MATRIX_INDICATOR_SET_COLOR(0, 255, 153, 51) //esc
+                    RGB_MATRIX_INDICATOR_SET_COLOR(1, 255, 153, 51) //`
+                    RGB_MATRIX_INDICATOR_SET_COLOR(7, 255, 153, 51)//1
+                    RGB_MATRIX_INDICATOR_SET_COLOR(2, 255, 153, 51) //tab
+                    ///darker orange keys
+                    RGB_MATRIX_INDICATOR_SET_COLOR(8, 255, 128, 0)//q
+                    RGB_MATRIX_INDICATOR_SET_COLOR(14, 255, 128, 0)//w
+                    RGB_MATRIX_INDICATOR_SET_COLOR(13, 255, 128, 0)//2
+                    RGB_MATRIX_INDICATOR_SET_COLOR(19, 255, 128, 0)//3
+                    RGB_MATRIX_INDICATOR_SET_COLOR(9, 255, 128, 0)//a
+                    RGB_MATRIX_INDICATOR_SET_COLOR(3, 255, 128, 0)//capslock
+                    RGB_MATRIX_INDICATOR_SET_COLOR(4, 255, 128, 0)//lshift
+                    RGB_MATRIX_INDICATOR_SET_COLOR(5, 255, 128, 0)//lctrl
+
+                    RGB_MATRIX_INDICATOR_SET_COLOR(28, 255, 51, 51) //f5
+                    RGB_MATRIX_INDICATOR_SET_COLOR(34, 255, 51, 51) //f6
+                    RGB_MATRIX_INDICATOR_SET_COLOR(39, 255, 51, 51) //f7
+                    RGB_MATRIX_INDICATOR_SET_COLOR(44, 255, 51, 51) //f8
+                    RGB_MATRIX_INDICATOR_SET_COLOR(96, 255, 51, 51) //enter
+                    RGB_MATRIX_INDICATOR_SET_COLOR(79, 255, 51, 51) //rightarrow
+                    RGB_MATRIX_INDICATOR_SET_COLOR(95, 255, 51, 51) //leftarrow
+                    RGB_MATRIX_INDICATOR_SET_COLOR(97, 255, 51, 51) //dwnarrow
+                    RGB_MATRIX_INDICATOR_SET_COLOR(94, 255, 51, 51) //uparrow
+                    //red keys
+                    RGB_MATRIX_INDICATOR_SET_COLOR(11, 255, 51, 51)//win
+                    RGB_MATRIX_INDICATOR_SET_COLOR(10, 255, 51, 51)//z
+                    RGB_MATRIX_INDICATOR_SET_COLOR(15, 255, 51, 51)//s
+                    RGB_MATRIX_INDICATOR_SET_COLOR(20, 255, 51, 51)//e
+                    RGB_MATRIX_INDICATOR_SET_COLOR(24, 255, 51, 51)//4
+                    //magenta
+                    RGB_MATRIX_INDICATOR_SET_COLOR(17, 204, 0, 102)//lalt
+                    RGB_MATRIX_INDICATOR_SET_COLOR(16, 204, 0, 102)//x
+                    RGB_MATRIX_INDICATOR_SET_COLOR(21, 204, 0, 102)//d
+                    RGB_MATRIX_INDICATOR_SET_COLOR(25, 204, 0, 102)//r
+                    RGB_MATRIX_INDICATOR_SET_COLOR(29, 204, 0, 102)//5
+                    //purple
+                    RGB_MATRIX_INDICATOR_SET_COLOR(22, 153, 0, 153)//c
+                    RGB_MATRIX_INDICATOR_SET_COLOR(26, 153, 0, 153)//f
+                    RGB_MATRIX_INDICATOR_SET_COLOR(30, 153, 0, 153)//t
+                    RGB_MATRIX_INDICATOR_SET_COLOR(35, 153, 0, 153)//6
+                    //darker blue
+                    RGB_MATRIX_INDICATOR_SET_COLOR(27, 76, 0, 153)//v
+                    RGB_MATRIX_INDICATOR_SET_COLOR(31, 76, 0, 153)//g
+                    RGB_MATRIX_INDICATOR_SET_COLOR(36, 76, 0, 153)//y
+                    RGB_MATRIX_INDICATOR_SET_COLOR(40, 76, 0, 153)//7
+                    */
+
+
+
+            break;
+
+            case 1:
+
+                 for (uint8_t i = 0; i < DRIVER_LED_TOTAL; i++)
+                    {
+                        RGB_MATRIX_INDICATOR_SET_COLOR(i,0,0,0);
+                    }
+                RGB_MATRIX_INDICATOR_SET_COLOR(0, 255, 153, 51) //esc
+                RGB_MATRIX_INDICATOR_SET_COLOR(6, 64, 64, 64) //f1
+                RGB_MATRIX_INDICATOR_SET_COLOR(8, 255, 128, 0)//q //q
+                RGB_MATRIX_INDICATOR_SET_COLOR(14, 255, 128, 0) //w
+                RGB_MATRIX_INDICATOR_SET_COLOR(20, 255, 51, 51) //e
+                RGB_MATRIX_INDICATOR_SET_COLOR(15, 255, 51, 51)//s
+                RGB_MATRIX_INDICATOR_SET_COLOR(26, 153, 0, 153)//f
+                RGB_MATRIX_INDICATOR_SET_COLOR(49, 64, 64, 64) //R_Alt
+            break;
+
+            default:
+                break;
+            break;
 }
 
-void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-    switch(get_highest_layer(layer_state)) {
-    // special handling per layer
-        case 0:  //layer one
-            if (host_keyboard_led_state().caps_lock)
-                rgb_matrix_set_color_all(255,0,0);
-            else
-                rgb_matrix_set_color_all(0,255,56);
-            break;
-        case 1:
-            for (uint8_t i = led_min; i < led_max; i++) {
-                 RGB_MATRIX_INDICATOR_SET_COLOR(i,0,0,0);
-            }
-            RGB_MATRIX_INDICATOR_SET_COLOR(0, 0, 0, 255) //esc
-            RGB_MATRIX_INDICATOR_SET_COLOR(6, 0, 0, 255) //f1
-            RGB_MATRIX_INDICATOR_SET_COLOR(8, 0, 0, 255) //q
-            RGB_MATRIX_INDICATOR_SET_COLOR(14, 0, 0, 255) //w
-            RGB_MATRIX_INDICATOR_SET_COLOR(20, 0, 0, 255) //e
-            RGB_MATRIX_INDICATOR_SET_COLOR(15, 0, 0, 255) //s
-            RGB_MATRIX_INDICATOR_SET_COLOR(26, 0, 0, 255) //f
-            RGB_MATRIX_INDICATOR_SET_COLOR(49, 0, 0, 255) //R_Alt
-            break;
-        default:
-            break;
-    }
 }
